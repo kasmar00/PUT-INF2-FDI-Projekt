ba51ca4235581fa2ea4c6e057e0e6278ba615a56
czarnobylu <jbylicki@hotmail.com>
2020-05-22 17:11:20 +0200

    changing to bylicki branch

--- layout.py
+++ layout.py
@@ -2,3 +2,5 @@ layout_name= "Wariat B"
 layout_screen_w=1600
 layout_screen_h=900
 vetical_bar_right_w_offset = layout_screen_w*0.8
+horizontal_bar_height_offset = layout_screen_h*0.75
+vertical_bar_left_w_offset = layout_screen_w*0.6
--- main.py
+++ main.py
@@ -3,10 +3,19 @@ from layout import *
 
 def main_loop(window):
 	#setup
-	window.setBackground("grey")
-	vert_divider_right = Rectangle(Point(vetical_bar_right_w_offset,0),Point(vetical_bar_right_w_offset,layout_screen_h))
+	window.setBackground(color_rgb(188,188,188))
+	vert_divider_right = Rectangle(Point(vetical_bar_right_w_offset,0),
+		Point(vetical_bar_right_w_offset,layout_screen_h))
 	vert_divider_right.setFill("black")
 	vert_divider_right.draw(window)
+	vert_divider_left = Rectangle(Point(vertical_bar_left_w_offset,0),
+		Point(vertical_bar_left_w_offset,layout_screen_h))
+	vert_divider_left.setFill("black")
+	vert_divider_left.draw(window)
+	horz_divider_down = Rectangle(Point(0,horizontal_bar_height_offset),
+		Point(vertical_bar_left_w_offset,horizontal_bar_height_offset))
+	horz_divider_down.setFill("black")
+	horz_divider_down.draw(window)
 	#loop
 	while not window.isClosed():
 		window.update()
