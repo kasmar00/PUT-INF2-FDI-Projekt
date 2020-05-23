34ac131676d6fd7af3a17da631d17b2ff09b58ce
Marcin_Kasznia <kasmar00@gmail.com>
2020-05-23 23:26:01 +0200

    add better collision (doesn't work)

--- atom.py
+++ atom.py
@@ -23,29 +23,32 @@ class atom:
 		d=5
 		dist=((self.x-other.x)**2+(self.y-other.y)**2)**0.5
 		if dist<d:
-			"""
-			#self
-			P1=array([other.x, other.y])
-			P2=array([self.x, self.y])
-			P3=array([self.x+self.Vx, self.y+self.Vy])
-			#print(P1,P2,P3)
-			P4=((P1-P2)*dot(P3-P2, P1-P2))/(dot(P1-P2, P1-P2))+P2
-			#print(P4)
-			snewvx, snewvy=-(P4-P2)
-			#other
-			P1=array([self.x, self.y])
-			P2=array([other.x, other.y])
-			P3=array([other.x+other.Vx, other.y+other.Vy])
-			#print(P1,P2,P3)
-			P4=((P1-P2)*dot(P3-P2, P1-P2))/(dot(P1-P2, P1-P2))+P2
-			#print(P4)
-			onewvx, onewvy=-(P4-P2)
-			self.Vx=onewvx
-			self.Vy=onewvy
-			other.Vx=snewvx
-			other.Vy=snewvy
-			"""
-			self.Vx,other.Vx=other.Vx,self.Vx
-			self.Vy,other.Vy=other.Vy,self.Vy
+			self_velocity_parallel=self.__get_projected_vector((other.x, other.y), (self.Vx, self.Vy))
+			other_velocity_parallel=other.__get_projected_vector((self.x, self.y), (other.Vx, other.Vy))
+			self_velocity_perpendicular=self.__get_projected_vector(self.__get_perp_vector(other), (self.Vx, self.Vy))
+			other_velocity_perpendicular=other.__get_projected_vector(other.__get_perp_vector(self), (other.Vx, other.Vy))
+			self.Vx, self.Vy=array(self_velocity_perpendicular)+array(other_velocity_parallel)
+			other.Vx, other.Vy=array(other_velocity_perpendicular)+array(self_velocity_parallel)
+			#self.Vx,other.Vx=other.Vx,self.Vx
+			#self.Vy,other.Vy=other.Vy,self.Vy
+
+	def __get_projected_vector(self, o, v):
+		ox,oy=o
+		x,y=v
+		P1=array([ox, oy])
+		P2=array([self.x, self.y])
+		P3=array([self.x+x, self.y+y])
+		P4=((P1-P2)*dot(P3-P2, P1-P2))/(dot(P1-P2, P1-P2))+P2
+		snewvx, snewvy=-(P4-P2)
+		return(snewvx, snewvy)
+
+	def __get_perp_vector(self, other):
+		sx, sy=self.x, self.y
+		ox, oy=other.x, other.y
+		onx=1+sx
+		print(sx, sy, ox, oy)
+		ony=((ox-sx)*(onx-sx))/(oy-sy)+sy
+		return(onx, ony)
+
 
 
