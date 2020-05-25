7e121a19eac98063d393325ddc33dfb83ef7f617
czarnobylu <jbylicki@hotmail.com>
2020-05-25 02:08:27 +0200

    previous-next-forward-reverse controls added

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from layout import *
 from box import *
 from control import *
 from time import sleep
+from cache import *
+from copy import deepcopy
+
 def setup_window(window):
 	window.setBackground(color_rgb(188,188,188))
 	vert_divider_right = Rectangle(Point(vetical_bar_right_w_offset,0),
@@ -18,6 +21,18 @@ def setup_window(window):
 	horz_divider_down.setFill("black")
 	horz_divider_down.draw(window)
 
+def step_back(cache,box,cache_size):
+	#delete_top kind of does not like smaller lists and tbh it is kinda useless to
+	#implemet an edge case just to get 2 more iterations of caching
+	if cache_size > 2:
+		temp = cache.head
+		while temp.next is not None:
+			temp = temp.next
+		box.atoms = deepcopy(temp.value)
+		box.update_visual_atoms()
+		cache.delete_top()
+		cache_size -=1
+	return cache_size
 
 def main_loop(window):
 	#setup
@@ -29,22 +44,66 @@ def main_loop(window):
 	box.add_atoms(num_atoms)
 	box.display_rect.setWidth(3)
 	box.display_rect.draw(window)
+	
+	#cache driver code:
+	cache_size = 0
+	max_cache_size = 500 #does not affect the speed of the simulation while providing a solid buffer imo
+	cache = List()
 
 	pauseButton = Button(1400,10,100,20,testButtonFunction,False,window)
 	pauseButton.rect.setFill("pink")
+	previousButton = Button(1360,10,20,20,testButtonFunction,False,window)
+	previousButton.rect.setFill("red")
+	reverseButton = Button(1320,10,20,20,testButtonFunction,False,window)
+	reverseButton.rect.setFill("black")
+	forwardButton = Button(1560,10,20,20,testButtonFunction,False,window)
+	forwardButton.rect.setFill("black")
+	nextButton = Button(1520,10,20,20,testButtonFunction,False,window)
+	nextButton.rect.setFill("red")
 	#loop
 	while not window.isClosed():
 		clickPoint = window.checkMouse()
 		if clickPoint is not None:
 			pauseButton.is_pressed(clickPoint.getX(),clickPoint.getY())
+			previousButton.is_pressed(clickPoint.getX(),clickPoint.getY())
+			nextButton.is_pressed(clickPoint.getX(),clickPoint.getY())
+			if pauseButton.var == True:
+				reverseButton.is_pressed(clickPoint.getX(),clickPoint.getY())
+			if pauseButton.var == False:
+				forwardButton.is_pressed(clickPoint.getX(),clickPoint.getY())
+			clickPoint = None
 			#later, invoke a global all-button check function
 		if pauseButton.var == False:
 			box.move_atoms()
+			cache.insert_top(Node(deepcopy(box.atoms)))
+			cache_size+=1
+			if cache_size > max_cache_size:
+				cache.delete_head()
+				cache_size-=1
 			box.check_collisions()
 			box.update_visual_atoms()
+		elif previousButton.var == True:	
+			cache_size = step_back(cache,box,cache_size)
+			previousButton.var = previousButton.f(previousButton.var)
+		elif nextButton.var == True:	
+			box.move_atoms()
+			cache.insert_top(Node(deepcopy(box.atoms)))
+			cache_size+=1
+			if cache_size > max_cache_size:
+				cache.delete_head()
+				cache_size-=1
+			box.check_collisions()
+			box.update_visual_atoms()
+			nextButton.var = nextButton.f(nextButton.var)
+		elif reverseButton.var == True:	
+			cache_size = step_back(cache,box,cache_size)
 		window.update()
-		sleep(0.01)
+		if forwardButton.var == True:	
+			pass
+		else:
+			sleep(0.01)
 	print("done")
+
 if __name__=="__main__":
 	#make a window
 	main_window = GraphWin(layout_name,layout_screen_w,layout_screen_h,autoflush=False)
