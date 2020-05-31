72387db75e082e897da2eee7f8bd21e48a3f52d8
czarnobylu <jbylicki@hotmail.com>
2020-05-31 17:44:09 +0200

    speed slider, sliders and layout changes

--- control.py
+++ control.py
@@ -14,6 +14,33 @@ class Button:
 			self.var = self.f(self.var)
 
 
+class Slider:
+	def __init__(self,pos,slen,var,vmin,vmax,win):
+		self.x, self.y = pos
+		self.len = slen
+		self.var = var
+		self.vmin = vmin
+		self.vmax = vmax
+		self.body = Rectangle(Point(self.x,self.y+10),Point(self.x+self.len,self.y+30-10))
+		self.body.setFill("grey")
+		self.body.draw(win)
+		self.wedge = Rectangle(Point(self.x + int(self.len*(self.var/abs(self.vmin-self.vmax)))-5,self.y),
+			Point(self.x + int(self.len*(self.var/abs(self.vmin-self.vmax)))+5,self.y+30))
+		self.wedge.setFill("orange")
+		self.wedge.draw(win)
+		self.win = win
+
+	def is_pressed(self,mouseX,mouseY):
+		if(mouseX > self.x and mouseX < self.x+self.len and mouseY > self.y and mouseY < self.y+30):
+			temp = self.var
+			self.var = int((self.vmax-self.vmin)*((mouseX-self.x)/self.len))+self.vmin
+			self.wedge.undraw()
+			print(self.var)
+			self.wedge = Rectangle(Point(self.x + int(self.len*((self.var-self.vmin)/abs(self.vmin-self.vmax)))-5,self.y),
+				Point(self.x + int(self.len*((self.var-self.vmin)/abs(self.vmin-self.vmax)))+5,self.y+30))
+			self.wedge.setFill("orange")
+			self.wedge.draw(self.win)
+	
 def testButtonFunction(state):
 	return not state
 
